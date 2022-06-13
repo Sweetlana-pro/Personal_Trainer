1774c1f20ca1d4ef9786d1ebe848d6cc7e60fd02
Sweetlana Protsenko <svitprotsenko@gmail.com>
2022-06-12 18:50:52 -0600

    aggregate queries

--- aggregate_queries.sql
+++ aggregate_queries.sql
@@ -121,12 +121,29 @@ ORDER BY Exercise.Name;
 -- Sample: 
 -- WorkoutName, EarliestBirthDate, LatestBirthDate
 -- '3, 2, 1... Yoga!', '1928-04-28', '1993-02-07'
+SELECT
+    w.Name,
+    MIN(c.BirthDate) EarliestBirthDate,
+    MAX(c.BirthDate) LatestBirthDate
+FROM Workout w
+INNER JOIN ClientWorkout cw ON w.WorkoutId = cw.WorkoutId
+INNER JOIN Client c ON c.ClientId = cw.ClientId
+GROUP BY w.Name
+ORDER BY w.Name;
+
 --------------------
 
 -- Count client goals.
 -- Be careful not to exclude rows for clients without goals.
 -- 500 rows total
 -- 50 rows with no goals
+SELECT
+    CONCAT(c.LastName, ' ', c.FirstName) ClientName,
+    COUNT(cg.GoalId) NumberOfGoals
+FROM ClientGoal cg
+RIGHT OUTER JOIN Client c ON cg.ClientId = c.ClientId
+GROUP BY c.ClientId
+ORDER BY COUNT(cg.GoalId) DESC;
 --------------------
 
 -- Select Exercise.Name, Unit.Name, 
@@ -134,23 +151,63 @@ ORDER BY Exercise.Name;
 -- for all exercises with a configured ExerciseInstanceUnitValue.
 -- Order by Exercise.Name, then Unit.Name.
 -- 82 rows
+SELECT
+    e.Name ExName,
+    u.Name UnitName,
+    MIN(eiuv.Value) MinValue,
+    MAX(eiuv.Value) MaximumValue
+FROM Exercise e 
+INNER JOIN ExerciseInstance ei ON e.ExerciseId = ei.ExerciseId
+INNER JOIN ExerciseInstanceUnitValue eiuv ON eiuv.ExerciseInstanceId = ei.ExerciseInstanceId
+INNER JOIN Unit u ON u.UnitId = eiuv.UnitId
+GROUP BY e.Name, e.ExerciseId, u.UnitId, u.Name
+ORDER BY e.Name, u.Name;
 --------------------
 
 -- Modify the query above to include ExerciseCategory.Name.
 -- Order by ExerciseCategory.Name, then Exercise.Name, then Unit.Name.
 -- 82 rows
+SELECT
+    ec.Name,
+    e.Name ExName,
+    u.Name UnitName,
+    MIN(eiuv.Value) MinValue,
+    MAX(eiuv.Value) MaximumValue
+FROM Exercise e 
+INNER JOIN ExerciseCategory ec ON ec.ExerciseCategoryId = e.ExerciseCategoryId
+INNER JOIN ExerciseInstance ei ON e.ExerciseId = ei.ExerciseId
+INNER JOIN ExerciseInstanceUnitValue eiuv ON eiuv.ExerciseInstanceId = ei.ExerciseInstanceId
+INNER JOIN Unit u ON u.UnitId = eiuv.UnitId
+GROUP BY ec.Name, ec.ExerciseCategoryId, e.Name, e.ExerciseId, u.UnitId, u.Name
+ORDER BY ec.Name, e.Name, u.Name;
 --------------------
 
 -- Select the minimum and maximum age in years for
 -- each Level.
 -- To calculate age in years, use the MySQL function DATEDIFF.
 -- 4 rows
+SELECT
+    l.Name LevelName,
+    MIN(DATEDIFF(CURDATE(), c.BirthDate) / 365) MinAge,
+    MAX(DATEDIFF(CURDATE(), c.BirthDate) / 365) MinAge
+FROM Level l 
+LEFT OUTER JOIN Workout w ON l.LevelId = w.LevelId
+INNER JOIN ClientWorkout cw ON cw.WorkoutId = w.WorkoutId
+INNER JOIN Client c ON c.ClientId = cw.ClientId  
+GROUP BY l.Name;  
+
 --------------------
 
 -- Stretch Goal!
 -- Count logins by email extension (.com, .net, .org, etc...).
 -- Research SQL functions to isolate a very specific part of a string value.
 -- 27 rows (27 unique email extensions)
+SELECT
+    SUBSTRING_INDEX(EmailAddress, '.', -1) Extention,
+    COUNT(EmailAddress) NumberOfEmails
+FROM Login
+GROUP BY SUBSTRING_INDEX(EmailAddress, '.', -1)
+ORDER BY COUNT(EmailAddress);
 --------------------
 
 -- Stretch Goal!
@@ -159,4 +216,16 @@ ORDER BY Exercise.Name;
 -- all workouts that match at least 2 of a client's goals.
 -- Order by the client's last name, then first name.
 -- 139 rows
+SELECT
+    CONCAT(c.LastName, ' ', c.FirstName) Client,
+    w.Name WorkoutName,
+    COUNT(cg.GoalId)
+FROM Client c
+INNER JOIN ClientGoal cg ON c.ClientId = cg.ClientId
+INNER JOIN WorkoutGoal wg ON cg.GoalId = wg.GoalId
+INNER JOIN Workout w ON wg.WorkoutId = w.WorkoutId
+GROUP BY w.WorkoutId, w.Name, c.ClientId, c.Lastname, c.FirstName
+HAVING COUNT(cg.GoalId) >= 2
+ORDER BY c.LastName, c.FirstName;
+
 --------------------
